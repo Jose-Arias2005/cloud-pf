3c13d33a469ed95fac859703c16d98abd97e3ae4
José Armando Arias Romero <166131122+Jose-Arias2005@users.noreply.github.com>
2024-12-01 15:19:50 -0500

    Update DeleteMovie.js

--- api-peliculas/DeleteMovie.js
+++ api-peliculas/DeleteMovie.js
@@ -4,9 +4,9 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 exports.handler = async (event) => {
     try {
         const body = JSON.parse(event.body); // Parsear el body de la solicitud
-        const { user_id, title, cinema_id } = body;
+        const { title, cinema_id } = body;
 
-        const requiredFields = ['user_id', 'cinema_id', 'title'];
+        const requiredFields = ['cinema_id', 'title'];
         for (let field of requiredFields) {
             if (!body[field]) {
                 return {
@@ -16,22 +16,6 @@ exports.handler = async (event) => {
             }
         }
 
-        // Verificar permisos del usuario
-        const t_usuarios = process.env.TABLE_NAME_USUARIOS;
-        const userResponse = await dynamodb
-            .get({
-                TableName: t_usuarios,
-                Key: { cinema_id, user_id },  // Usamos cinema_id y user_id como claves
-            })
-            .promise();
-
-        // Verificar permisos del usuario y si el cinema_id del usuario coincide con el cinema_id del cuerpo de la solicitud
-        if (!userResponse.Item || userResponse.Item.role !== 'admin' || userResponse.Item.cinema_id !== cinema_id) {
-            return {
-                statusCode: 403,
-                body: JSON.stringify({ error: 'Permiso denegado o el usuario no tiene acceso a este cine' }),
-            };
-        }
 
         // Eliminar la película (utilizando cinema_id y title como claves)
         const t_peliculas = process.env.TABLE_NAME_PELICULAS;
